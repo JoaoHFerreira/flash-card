4a68324a2c9abaebf6d1c9027c0f0eb531c39e6b
JoaoHFerreira <jaycops@gmail.com>
2024-09-17 17:49:04 +0100

    update model, schema, lib and bin in order to create action write_subject_learn

--- app/src/models.rs
+++ app/src/models.rs
@@ -1,5 +1,7 @@
 use diesel::prelude::*;
-use crate::schema::posts;
+use crate::schema::{posts};
+use crate::schema::{learning_topic};
+
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::posts)]
@@ -11,12 +13,23 @@ pub struct Post {
     pub published: bool,
 }
 
-
-
-
 #[derive(Insertable)]
 #[diesel(table_name = posts)]
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::learning_topic)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct LearningTopic {
+    pub id: i32,
+    pub subject: String,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = learning_topic)]
+pub struct NewLearningTopic<'a> {
+    pub subject: &'a str,
 }
--- app/src/schema.rs
+++ app/src/schema.rs
@@ -1,3 +1,31 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    flash_card (id) {
+        id -> Int4,
+        question -> Text,
+        answer -> Text,
+        learning_topic_id -> Int4,
+        practice_schedule_id -> Int4,
+    }
+}
+
+diesel::table! {
+    historical_acceptances (id) {
+        id -> Int4,
+        flash_card_id -> Int4,
+        is_right -> Bool,
+        test_date -> Timestamp,
+    }
+}
+
+diesel::table! {
+    learning_topic (id) {
+        id -> Int4,
+        subject -> Text,
+    }
+}
+
 diesel::table! {
     posts (id) {
         id -> Int4,
@@ -6,3 +34,23 @@ diesel::table! {
         published -> Bool,
     }
 }
+
+diesel::table! {
+    practice_schedule (id) {
+        id -> Int4,
+        current_practice_day -> Timestamp,
+        next_practice_day -> Timestamp,
+    }
+}
+
+diesel::joinable!(flash_card -> learning_topic (learning_topic_id));
+diesel::joinable!(flash_card -> practice_schedule (practice_schedule_id));
+diesel::joinable!(historical_acceptances -> flash_card (flash_card_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    flash_card,
+    historical_acceptances,
+    learning_topic,
+    posts,
+    practice_schedule,
+);
