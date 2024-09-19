b7e0a53d34ef50568235db5cb75c2010e30f2b7e
JoaoHFerreira <jaycops@gmail.com>
2024-09-19 18:02:46 +0100

    adding show flashcards

--- app/src/bin/show_flash_cards.rs
+++ app/src/bin/show_flash_cards.rs
@@ -3,19 +3,27 @@ use diesel::prelude::*;
 use flash_card::*;
 
 
-fn main(){
+fn main() {
     use self::schema::flash_card::dsl::*;
+    use self::schema::practice_schedule::dsl::*;
+    use diesel::dsl::sql;
 
     let connection = &mut establish_connection();
-
+    let limit = 30;
+    
     let results = flash_card
-        .limit(30)
+        .inner_join(practice_schedule)
+        .order_by(sql::<diesel::sql_types::Text>("RANDOM()"))
+        .limit(limit)
         .select(FlashCard::as_select())
-        .load(connection)
-        .expect("Error loading flash cards");
+        .load::<FlashCard>(connection)
+        .expect("Error loading inner join");
 
-    for card in results{
-        println!("{}", card.question);
+    for card in results {
+        println!("ID: {}", card.id);
+        println!("Question: {}", card.question);
+        println!("Answer: {}", card.answer);
+        println!("Practice Schedule ID: {}", card.practice_schedule_id);
+        println!("--------------------");
     }
-
 }
